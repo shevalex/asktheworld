d5b62d878e7f5bf2d0ecaf4b9c08f1f266eaf40f
Oleg Burakov <Advocate@Olegs-MacBook-Air.local>
2014-12-10 12:07:35 -0500

    Initial Commit

---
+++ AskTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AskTheWorld
+//
+//  Created by Oleg Burakov on 10/12/14.
+//  Copyright (c) 2014 Oleg Burakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
