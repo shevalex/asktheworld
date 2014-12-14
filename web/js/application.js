54350d0ea34460aee905afe0e2625664d6477f0b
aavtamon <anton.avtamonov@gmail.com>
2014-12-14 00:35:23 -0500

    some improvements in registration process

--- web/js/application.js
+++ web/js/application.js
@@ -16,6 +16,18 @@ Application.initializeToolbar = function() {
     });
   });
 
+  $("#TopLevelToolbar-Profile").click(function(event) { 
+    if (!($("#ModifyUserProfilePanel").is(":visible"))) {
+      var userProfile = Backend.getUserProfile();
+      $("#ModifyUserProfilePanel-Password").val(userProfile.password);
+      $("#ModifyUserProfilePanel-RetypePassword").val(userProfile.password);
+      $("#ModifyUserProfilePanel-Name").val(userProfile.name);
+      $("#ModifyUserProfilePanel-Gender").val(userProfile.gender);
+      $("#ModifyUserProfilePanel-AgeCategory").val(userProfile.age);
+    }
+    $("#ModifyUserProfilePanel").slideToggle("slow");
+  });
+
   $("#LoginPanel-Register").click(function(event) { 
     $("#LoginPanel").slideUp("fast", function() {
       $("#RegisterUserPanel").slideDown("slow");
@@ -30,17 +42,30 @@ Application.initializeToolbar = function() {
     Application.register();
   });
 
+  $("#ModifyUserProfilePanel-Update").click(function(event) { 
+    Application.updateProfile();
+  });
+
   var hideListener = function(panelName, event) {
-    var container = $("#" + panelName);
+    var isEventTarget = function(container, event) {
+      return container.is(event.target) || container.has(event.target).length > 0;
+    }
+
+    if (isEventTarget($("#TopLevelToolbar-Login"), event) || isEventTarget($("#TopLevelToolbar-Profile"), event)) {
+      return;
+    }
+
+    var container = $(panelName);
     if (container.is(":visible")) {
-      if (!container.is(event.target) && container.has(event.target).length === 0) {
+      if (!isEventTarget(container, event)) {
         container.slideUp("fast");
       }
     }
   };
 
-  $(document).mouseup(hideListener.bind(this, "LoginPanel"));
-  $(document).mouseup(hideListener.bind(this, "RegisterUserPanel"));
+  $(document).mouseup(hideListener.bind(this, "#LoginPanel"));
+  $(document).mouseup(hideListener.bind(this, "#RegisterUserPanel"));
+  $(document).mouseup(hideListener.bind(this, "#ModifyUserProfilePanel"));
 }
 
 
@@ -49,7 +74,10 @@ Application.signIn = function() {
   var password = $("#LoginPanel-Password").val();
 
   if (Backend.verifyUserIdentity(email, password)) {
+    $("#TopLevelToolbar-UserName").text("Welcome, " + email);
     $("#LoginPanel").slideUp("fast");
+    $("#TopLevelToolbar-Login").hide();
+    $("#TopLevelToolbar-Profile").show();
   } else {
     //highlight incorect password
   }
@@ -58,10 +86,25 @@ Application.signIn = function() {
 Application.register = function() {
   var email = $("#RegisterUserPanel-Email").val();
   var password = $("#RegisterUserPanel-Password").val();
+  var name = $("#RegisterUserPanel-Name").val();
   var gender = $("#RegisterUserPanel-Gender").val();
+  var age = $("#RegisterUserPanel-AgeCategory").val();
 
-  if (Backend.verifyUserIdentity(email, password, gender)) {
+  if (Backend.registerUser({"email": email, "password": password, "gender": gender, "age": age, "name": name})) {
     $("#RegisterUserPanel").slideUp("fast");
+  } else {
+    //indicate what is incorrect
+  }
+}
+
+Application.updateProfile = function() {
+  var password = $("#RegisterUserPanel-Password").val();
+  var name = $("#RegisterUserPanel-Name").val();
+  var gender = $("#RegisterUserPanel-Gender").val();
+  var age = $("#RegisterUserPanel-AgeCategory").val();
+
+  if (Backend.updateUser({"password": password, "gender": gender, "age": age, "name": name})) {
+    $("#ModifyUserProfilePanel").slideUp("fast");
   } else {
     //indicate what is incorrect
   }
--- web/js/backend.js
+++ web/js/backend.js
@@ -1,10 +1,35 @@
 
 var Backend = {};
 
+Backend.UserProfile = {"email": null, "password": null, "gender": null, "age": null, "name": null};
+
 Backend.verifyUserIdentity = function(email, password) {
   return true;
 }
 
-Backend.registerUser = function(email, password, gender) {
+Backend.registerUser = function(userProfile) {
+  this.UserProfile.email = userProfile.email;
+  this.UserProfile.password = userProfile.password;
+  this.UserProfile.gender = userProfile.gender;
+  this.UserProfile.age = userProfile.age;
+  this.UserProfile.name = userProfile.name;
   return true;
 }
+
+Backend.updateUser = function(userProfile) {
+  this.UserProfile.password = userProfile.password;
+  this.UserProfile.gender = userProfile.gender;
+  this.UserProfile.age = userProfile.age;
+  this.UserProfile.name = userProfile.name;
+  return true;
+}
+
+
+Backend.getUserProfile = function() {
+  if (true) {
+    return this.UserProfile;
+  } else {
+    return null;
+  }
+}
+
