7062755c3b1802050a97619b42d4cd892ede5458
Tarkit <rfb384@localhost.localdomain>
2014-12-15 16:22:55 -0500

    Fix https://github.com/shevalex/asktheworld/issues/2  Adding Content-Type, Location, Allow to Access-Control-Allow-Headers

--- server/src/main/java/com/pisoft/asktheworld/filters/CROSFilter.java
+++ server/src/main/java/com/pisoft/asktheworld/filters/CROSFilter.java
@@ -22,7 +22,7 @@ public class CROSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Location, Allow");
 		chain.doFilter(req, res);
 	}
 
