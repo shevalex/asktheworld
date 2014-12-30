891786b1e39aa1e7a233423875b6f957c277f912
Tarkit <rfb384@localhost.localdomain>
2014-12-29 19:44:39 -0500

    add Location header to Expose list for CORS

--- server/src/main/java/com/pisoft/asktheworld/filters/CROSFilter.java
+++ server/src/main/java/com/pisoft/asktheworld/filters/CROSFilter.java
@@ -23,6 +23,7 @@ public class CROSFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Content-Type, Location, Allow, Token");
+		response.setHeader("Access-Control-Expose-Headers", "Location");
 		chain.doFilter(req, res);
 	}
 
