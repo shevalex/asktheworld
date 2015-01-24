c960e74a979304570dd84a7f3628d7ebfc142c0d
aavtamon <anton.avtamonov@gmail.com>
2015-01-24 12:47:08 -0500

    removed list from the all-requests page

--- web/pages/all_outgoing_requestspage.js
+++ web/pages/all_outgoing_requestspage.js
@@ -3,8 +3,6 @@ AllOutgoingRequestsPage = ClassUtils.defineClass(AbstractRequestPage, function A
   
   this._requestTable = null;
   this._requestTableContainer = null;
-  this._requestsContainer = null;
-  this._requestList = null;
 });
 
 AllOutgoingRequestsPage.prototype.definePageContent = function(root) {
@@ -18,58 +16,35 @@ AllOutgoingRequestsPage.prototype.definePageContent = function(root) {
   });
   
   this._requestTableContainer = UIUtils.appendBlock(root, "TablePanel");
-  this._requestsContainer = UIUtils.appendBlock(root, "RequestPanel");
-}
 
-AllOutgoingRequestsPage.prototype.onShow = function(root) {
-  var updateListener = {
-    updateStarted: function() {
-      Application.showSpinningWheel();
-    },
-    updateFinished: function() {
-      Application.hideSpinningWheel();
-    }
-  }
-  
   this._requestTable = new AbstractRequestPage.OutgoingRequestsTable({
     requestStatus: null,
     selectionObserver: function(requestId) {
-      if (this._requestList != null) {
-        this._requestList.destroy();
+      var paramBundle = {
+        returnPageId: MenuPage.prototype.ALL_REQUESTS_ITEM_ID,
+        requestId: requestId,
+        otherRequestIds: null
       }
-      
-      this._requestList = new AbstractRequestPage.OutgoingRequestList({
-        requestClickListener: function(requestId) {
-          var paramBundle = {
-            returnPageId: MenuPage.prototype.ALL_REQUESTS_ITEM_ID,
-            requestId: requestId,
-            otherRequestIds: Backend.getOutgoingRequestIds()
-          }
 
-          Application.getMenuPage().showPage(MenuPage.prototype.REQUEST_DETAILS_PAGE_ID, paramBundle);
-        },
-        requestIds: [requestId],
-        requestEditable: true,
-        maxResponses: 3,
-        responseAreaMaxHeight: -1,
-        unviewedResponsesOnly: false,
-        updateListener: updateListener
-      });
-      
-      this._requestList.append(this._requestsContainer);
+      Application.getMenuPage().showPage(MenuPage.prototype.REQUEST_DETAILS_PAGE_ID, paramBundle);
     }.bind(this),
-    updateListener: updateListener
+    updateListener: {
+      updateStarted: function() {
+        Application.showSpinningWheel();
+      },
+      updateFinished: function() {
+        Application.hideSpinningWheel();
+      }
+    }
   });
   
   this._requestTable.append(this._requestTableContainer);
 }
 
+AllOutgoingRequestsPage.prototype.onShow = function(root) {
+}
+
 AllOutgoingRequestsPage.prototype.onHide = function() {
-  this._requestTable.remove();
-  
-  if (this._requestList != null) {
-    this._requestList.destroy();
-  }
 }
 
 
