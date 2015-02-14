c8e4bb43469cad35204250c08bd93f24b3a38b81
aavtamon <anton.avtamonov@gmail.com>
2015-02-13 23:55:23 -0500

    localized Application

--- web/js/application.js
+++ web/js/application.js
@@ -1,64 +1,64 @@
 
 var Application = {
   Configuration: {
-    LANGUAGES: [{data: "rus", display: "Russian"}, {data: "eng", display: "English"}, {data: "ger", display: "German"}, {data: "esp", display: "Spanish"}, {data: "fra", display: "French"}, {data: "por", display: "Portugeese"}, {data: "grc", display: "Greece"}, {data: "gon", display: "Gondurasee"}],
+    LANGUAGES: [{data: "rus", display: I18n.getLocale().literals.LanguageRussian}, {data: "eng", display: I18n.getLocale().literals.LanguageEnglish}, {data: "ger", display: I18n.getLocale().literals.LanguageGerman}, {data: "esp", display: I18n.getLocale().literals.LanguageSpanish}, {data: "fra", display: I18n.getLocale().literals.LanguageFrench}, {data: "por", display: I18n.getLocale().literals.LanguagePortugeese}, {data: "grc", display: I18n.getLocale().literals.LanguageGreece}, {data: "gon", display: I18n.getLocale().literals.LanguageGondurasee}],
     
-    AGE_CATEGORIES: [{data: "child", display: "Child"}, {data: "teenager", display: "Teenager"}, {data: "young", display: "Young Adult"}, {data: "adult", display: "Adult"}, {data: "senior", display: "Senior"}],
-    RESPONSE_WAIT_TIME: [{data: 148, display: "1 week"}, {data: 24, display: "1 day"}, {data: 12, display: "half-day"}, {data: 1, display: "1 hour"}],
-    RESPONSE_QUANTITY: [{data: -1, display: "As many as arrive"}, {data: 5, display: "Five"}, {data: 10, display: "Ten"}, {data: 3, display: "Three"}, {data: 1, display: "Just the first one"}],
-    GENDERS: [{data: "male", display: "Male"}, {data: "female", display: "Female"}],
-    AGE_CATEGORY_PREFERENCE: [{data: "all", display: "All"}, {data: "children", display: "Children"}, {data: "teenagers", display: "Teenagers"}, {data: "young", display: "Youngs"}, {data: "adults", display: "Adults"}, {data: "seniors", display: "Seniors"}],
-    GENDER_PREFERENCE: [{data: "any", display: "Any"}, {data: "male", display: "Male"}, {data: "female", display: "Female"}],
-    INQUIRY_LIMIT_PREFERENCE: [{data: -1, display: "As many as possible"}, {data: 10, display: "No more than ten"}, {data: 5, display: "No more than five"}, {data: 0, display: "I don't want to get any inquiries"}],
+    AGE_CATEGORIES: [{data: "child", display: I18n.getLocale().literals.AgeChild}, {data: "teenager", display: I18n.getLocale().literals.AgeTeenager}, {data: "young", display: I18n.getLocale().literals.AgeYoung}, {data: "adult", display: I18n.getLocale().literals.AgeAdult}, {data: "senior", display: I18n.getLocale().literals.AgeSenior}],
+    RESPONSE_WAIT_TIME: [{data: 148, display: I18n.getLocale().literals.WaitTimeWeek}, {data: 24, display: I18n.getLocale().literals.WaitTimeDay}, {data: 12, display: I18n.getLocale().literals.WaitTimeHalfDay}, {data: 1, display: I18n.getLocale().literals.WaitTimeHour}],
+    RESPONSE_QUANTITY: [{data: -1, display: I18n.getLocale().literals.QuantityAll}, {data: 5, display: I18n.getLocale().literals.QuantityFive}, {data: 10, display: I18n.getLocale().literals.QuantityTen}, {data: 3, display: I18n.getLocale().literals.QuantityThree}, {data: 1, display: I18n.getLocale().literals.QuantityOne}],
+    GENDERS: [{data: "male", display: I18n.getLocale().literals.GenderMale}, {data: "female", display: I18n.getLocale().literals.GenderFemale}],
+    AGE_CATEGORY_PREFERENCE: [{data: "all", display: I18n.getLocale().literals.AgePreferenceAll}, {data: "children", display: I18n.getLocale().literals.AgePreferenceChildren}, {data: "teenagers", display: I18n.getLocale().literals.AgePreferenceTeenagers}, {data: "young", display: I18n.getLocale().literals.AgePreferenceYoungs}, {data: "adults", display: I18n.getLocale().literals.AgePreferenceAdults}, {data: "seniors", display: I18n.getLocale().literals.AgePreferenceSeniors}],
+    GENDER_PREFERENCE: [{data: "any", display: I18n.getLocale().literals.GenderPreferenceAny}, {data: "male", display: I18n.getLocale().literals.GenderPreferenceMale}, {data: "female", display: I18n.getLocale().literals.GenderPreferenceFemale}],
+    INQUIRY_LIMIT_PREFERENCE: [{data: -1, display: I18n.getLocale().literals.IncomingLimitPreferenceAll}, {data: 10, display: I18n.getLocale().literals.IncomingLimitPreferenceTen}, {data: 5, display: I18n.getLocale().literals.IncomingLimitPreferenceFive}, {data: 0, display: I18n.getLocale().literals.IncomingLimitPreferenceNone}],
     
     toTargetGroupString: function (ageCategory, gender) {
       if (ageCategory == this.AGE_CATEGORY_PREFERENCE[0].data) {
         if (gender == this.GENDER_PREFERENCE[0].data) {
-          return "all people";
+          return I18n.getLocale().literals.TargetGroupAll;
         } else if (gender == this.GENDER_PREFERENCE[1].data) {
-          return "all men";
+          return I18n.getLocale().literals.TargetGroupAllMen;
         } else if (gender == this.GENDER_PREFERENCE[2].data) {
-          return "all women";
+          return I18n.getLocale().literals.TargetGroupAllWomen;
         }
       } else if (ageCategory == this.AGE_CATEGORY_PREFERENCE[1].data) {
         if (gender == this.GENDER_PREFERENCE[0].data) {
-          return "all children";
+          return I18n.getLocale().literals.TargetGroupAllChildren;
         } else if (gender == this.GENDER_PREFERENCE[1].data) {
-          return "all boys";
+          return I18n.getLocale().literals.TargetGroupAllBoys;
         } else if (gender == this.GENDER_PREFERENCE[2].data) {
-          return "all girls";
+          return I18n.getLocale().literals.TargetGroupAllGirls;
         }
       } else if (ageCategory == this.AGE_CATEGORY_PREFERENCE[2].data) {
         if (gender == this.GENDER_PREFERENCE[0].data) {
-          return "all teenagers";
+          return I18n.getLocale().literals.TargetGroupAllTeenagers;
         } else if (gender == this.GENDER_PREFERENCE[1].data) {
-          return "all teen guys";
+          return I18n.getLocale().literals.TargetGroupAllTeenBoys;
         } else if (gender == this.GENDER_PREFERENCE[2].data) {
-          return "all teen girls";
+          return I18n.getLocale().literals.TargetGroupAllTeenGirls;
         }
       } else if (ageCategory == this.AGE_CATEGORY_PREFERENCE[3].data) {
         if (gender == this.GENDER_PREFERENCE[0].data) {
-          return "all young people";
+          return I18n.getLocale().literals.TargetGroupAllYoungs;
         } else if (gender == this.GENDER_PREFERENCE[1].data) {
-          return "all young men";
+          return I18n.getLocale().literals.TargetGroupAllYoungMen;
         } else if (gender == this.GENDER_PREFERENCE[2].data) {
-          return "all young women";
+          return I18n.getLocale().literals.TargetGroupAllYoungWomen;
         }
       } else if (ageCategory == this.AGE_CATEGORY_PREFERENCE[4].data) {
         if (gender == this.GENDER_PREFERENCE[0].data) {
-          return "all adult people";
+          return I18n.getLocale().literals.TargetGroupAllAdults;
         } else if (gender == this.GENDER_PREFERENCE[1].data) {
-          return "all adult men";
+          return I18n.getLocale().literals.TargetGroupAllAdultMen;
         } else if (gender == this.GENDER_PREFERENCE[2].data) {
-          return "all adult women";
+          return I18n.getLocale().literals.TargetGroupAllAdultWomen;
         }
       } else if (ageCategory == this.AGE_CATEGORY_PREFERENCE[5].data) {
         if (gender == this.GENDER_PREFERENCE[0].data) {
-          return "all senior people";
+          return I18n.getLocale().literals.TargetGroupAllSeniors;
         } else if (gender == this.GENDER_PREFERENCE[1].data) {
-          return "all senior men";
+          return I18n.getLocale().literals.TargetGroupAllSeniorMen;
         } else if (gender == this.GENDER_PREFERENCE[2].data) {
-          return "all senior women";
+          return I18n.getLocale().literals.TargetGroupAllSeniorWomen;
         }
       }
       
@@ -68,33 +68,33 @@ var Application = {
     toUserIdentityString: function (ageCategory, gender) {
       if (ageCategory == this.AGE_CATEGORIES[0].data) {
         if (gender == this.GENDERS[0].data) {
-          return "boy";
+          return I18n.getLocale().literals.UserIdentityBoy;
         } else if (gender == this.GENDERS[1].data) {
-          return "girl";
+          return I18n.getLocale().literals.UserIdentityGirl;
         }
       } else if (ageCategory == this.AGE_CATEGORIES[1].data) {
         if (gender == this.GENDERS[0].data) {
-          return "teen boy";
+          return I18n.getLocale().literals.UserIdentityTeenBoy;
         } else if (gender == this.GENDERS[1].data) {
-          return "teen girl";
+          return I18n.getLocale().literals.UserIdentityTeenGirl;
         }
       } else if (ageCategory == this.AGE_CATEGORIES[2].data) {
         if (gender == this.GENDERS[0].data) {
-          return "young man";
+          return I18n.getLocale().literals.UserIdentityYoungMan;
         } else if (gender == this.GENDERS[1].data) {
-          return "young woman";
+          return I18n.getLocale().literals.UserIdentityYoungWoman;
         }
       } else if (ageCategory == this.AGE_CATEGORIES[3].data) {
         if (gender == this.GENDERS[0].data) {
-          return "man";
+          return I18n.getLocale().literals.UserIdentityAdultMan;
         } else if (gender == this.GENDERS[1].data) {
-          return "woman";
+          return I18n.getLocale().literals.UserIdentityAdultWoman;
         }
       } else if (ageCategory == this.AGE_CATEGORIES[4].data) {
         if (gender == this.GENDERS[0].data) {
-          return "senior man";
+          return I18n.getLocale().literals.UserIdentitySeniorMan;
         } else if (gender == this.GENDERS[1].data) {
-          return "senior woman";
+          return I18n.getLocale().literals.UserIdentitySeniorWoman;
         }
       }
       
@@ -217,7 +217,7 @@ Application.hideMessage = function() {
 Application.showDialog = function(title, contentHtml) {
   Application.hideDialog();
   
-  $("body").append("<div class='modal-dialog' id='ModalDialog'><div class='modal-dialog-content'>" + contentHtml + "</div><hr><div class='modal-dialog-controlpanel'><button class='modal-dialog-okbutton'>OK</button><div></div>");
+  $("body").append("<div class='modal-dialog' id='ModalDialog'><div class='modal-dialog-content'>" + contentHtml + "</div><hr><div class='modal-dialog-controlpanel'><button class='modal-dialog-okbutton'>" + I18n.getLocale().literarals.OkButton + "</button><div></div>");
   $(".modal-dialog-okbutton").click(function() {
     $(".modal-dialog").fadeOut("slow", function() {
       $(this).remove();
