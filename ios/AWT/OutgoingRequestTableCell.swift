847b2efe9ecb9db987808ec1e669c46ec504cf9f
Oleg <Advocate@Olegs-MacBook-Air.local>
2015-03-17 22:53:02 -0400

    OutgoingRequestTableCell added

---
+++ ios/AWT/OutgoingRequestTableCell.swift
@@ -0,0 +1,24 @@
+//
+//  OutgoingRequestTableCell.swift
+//  AWT
+//
+//  Created by Oleg Burakov on 17/03/15.
+//  Copyright (c) 2015 PiSoft Corporation. All rights reserved.
+//
+
+import UIKit
+
+class OutgoingRequestTableCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
