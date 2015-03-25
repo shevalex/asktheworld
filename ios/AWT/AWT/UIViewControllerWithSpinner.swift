cb8ac0793b600b65ae273133cc5f847742e56336
aavtamon <anton.avtamonov@gmail.com>
2015-03-25 00:17:07 -0400

    introduced spinner view. Supported listener removal

---
+++ ios/AWT/AWT/UIViewControllerWithSpinner.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewControllerWithSpinner.swift
+//  AWT
+//
+//  Created by Anton Avtamonov on 3/24/15.
+//  Copyright (c) 2015 PiSoft Corporation. All rights reserved.
+//
+
+import Foundation
