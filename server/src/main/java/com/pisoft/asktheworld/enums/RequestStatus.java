9f588841d3670f126a6adae94cb82ea4846875b2
Alexxx <ashevchenko@DELLL.local>
2015-05-14 00:29:00 -0400

    update enum to use it for json serialization/deserialization

--- server/src/main/java/com/pisoft/asktheworld/enums/RequestStatus.java
+++ server/src/main/java/com/pisoft/asktheworld/enums/RequestStatus.java
@@ -1,11 +1,18 @@
 package com.pisoft.asktheworld.enums;
 
+import java.util.EnumSet;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum RequestStatus {
 	ACTIVE("active"),
 	EXPIRED("expired"),
 	DELETED("deleted"),
 	ALL("all");
-	
+		
 	private RequestStatus(String s) {
 		status = s;
 	}
@@ -16,4 +23,21 @@ public enum RequestStatus {
 	}
 	
 	private final String status;
+	
+	private static Map<String, RequestStatus> map = new HashMap<String, RequestStatus>();
+
+	static {
+		   for(RequestStatus s : EnumSet.allOf(RequestStatus.class))
+               map.put(s.status, s);
+	}
+
+	@JsonCreator
+	public static RequestStatus forValue(String value) {
+	    return map.get(value);
+	}
+
+	@JsonValue
+	public String toValue() {
+		return status;
+	}
 }
