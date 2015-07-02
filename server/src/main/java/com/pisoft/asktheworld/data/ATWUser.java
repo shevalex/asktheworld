042990984b1fe13555cb3792fc8075db9792a4e2
Project <project@Tarkits-MacBook-Air.local>
2015-07-02 01:03:03 -0400

    Add support for request delition (Join table problem) Workaround. Need correct anotation

--- server/src/main/java/com/pisoft/asktheworld/data/ATWUser.java
+++ server/src/main/java/com/pisoft/asktheworld/data/ATWUser.java
@@ -5,14 +5,22 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.ConstraintMode;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.UniqueConstraint;
+import javax.validation.Constraint;
+
+import org.hibernate.annotations.Cascade;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pisoft.asktheworld.enums.Gender;
@@ -36,7 +44,7 @@ public class ATWUser implements Serializable {
 	private String password;
 	@Column
 	private String languages[];
-	@Column(columnDefinition="default 2") //TODO: need to add default value 
+	@Column(columnDefinition="int4 default 2 not null") //TODO: need to add default value 
 	private Gender gender = Gender.OTHER; //TODO: default value is not working
 	@Column
 	private String name;
@@ -83,9 +91,28 @@ public class ATWUser implements Serializable {
 	//TODO: Probably we need to change this to request IDs to minimize size of this array
 	@JsonIgnore
 	@ManyToMany
-	@JoinTable(name="USER_INC_REQ",
-	      joinColumns={@JoinColumn(name="USER_ID", referencedColumnName="id")},
-	      inverseJoinColumns={@JoinColumn(name="REQ_ID", referencedColumnName="request_id")})
+	@JoinTable(name="USER_INC_REQ",  
+	      joinColumns=@JoinColumn(name="USER_ID", referencedColumnName="id"), inverseForeignKey=@ForeignKey(value=ConstraintMode.NO_CONSTRAINT),
+	      inverseJoinColumns = @JoinColumn(name="REQ_ID", referencedColumnName="request_id", foreignKey=@ForeignKey(value=ConstraintMode.NO_CONSTRAINT), columnDefinition="int4 REFERENCES ATWRequest(request_id) ON DELETE CASCADE") ) //TODO: I am not sure that this will work for not postgres  
+//TODO: the result of this code is below. We dont need to alter foreign key REQ_ID. It is described in table creation 
+/**
+Hibernate: 
+    create table USER_INC_REQ (
+        USER_ID int4 not null,
+        REQ_ID int4 REFERENCES ATWRequest(request_id) ON DELETE CASCADE not null
+    )
+Hibernate: 
+    alter table USER_INC_REQ 
+        add constraint FK_3blnj2ryhscr9k4fqayn3inx8 
+        foreign key (REQ_ID) 
+        references ATWRequest
+Hibernate: 
+    alter table USER_INC_REQ 
+        add constraint FK_tampvlptvfb2r2ikovgk87ybm 
+        foreign key (USER_ID) 
+        references ATWUser	
+ */
+	
 	private List<ATWRequest> incomingRequets = new ArrayList<ATWRequest>();
 	
 	
