514d30cfe8c5fc82d13fd22119d13c34cbdd13df
Project <project@Tarkits-MacBook-Air.local>
2015-11-09 18:19:03 -0500

    Fix incorrect template variable #51

--- server/src/main/java/com/pisoft/asktheworld/controllers/UserSettingsController.java
+++ server/src/main/java/com/pisoft/asktheworld/controllers/UserSettingsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pisoft.asktheworld.data.ATWSettings;
 import com.pisoft.asktheworld.data.ATWUserSettings;
 import com.pisoft.asktheworld.data.DB;
 
@@ -19,17 +20,25 @@ public class UserSettingsController {
 	private DB db;
 
 	@Secured({"ROLE_ADMIN","ROLE_REAL_USER"})
-	@RequestMapping(method = RequestMethod.PUT, value="/user/{userID}/settings")
-	public ResponseEntity<ATWUserSettings> updateUserSettings(@PathVariable("userID") int id, @RequestBody ATWUserSettings settings) {
-		settings.setId(id);
-		settings = db.updateUserSettings(settings);
-		return new ResponseEntity<ATWUserSettings>(settings, settings != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+	@RequestMapping(method = RequestMethod.PUT, value="/user/{userID}/preferences")
+	public ResponseEntity<ATWUserSettings> updateUserPreferences(@PathVariable("userID") int id, @RequestBody ATWUserSettings preferences) {
+		preferences.setId(id);
+		preferences = db.updateUserPreferences(preferences);
+		return new ResponseEntity<ATWUserSettings>(preferences, preferences != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
 	}
 	
 	@Secured({"ROLE_ADMIN","ROLE_REAL_USER"})
-	@RequestMapping(method = RequestMethod.GET, value="/user/{userID}/settings")
-	public ResponseEntity<ATWUserSettings> updateUserSettings(@PathVariable("userID") int id) {
-		ATWUserSettings settings = db.getUserSettings(id);
-		return new ResponseEntity<ATWUserSettings>(settings, settings != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+	@RequestMapping(method = RequestMethod.GET, value="/user/{userID}/preferences")
+	public ResponseEntity<ATWUserSettings> getUserPreferences(@PathVariable("userID") int id) {
+		ATWUserSettings preferences = db.getUserPreferences(id);
+		return new ResponseEntity<ATWUserSettings>(preferences, preferences != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
 	}
+	
+	@Secured({"ROLE_ADMIN","ROLE_REAL_USER"})
+	@RequestMapping(method = RequestMethod.GET, value="user/{userID}/settings")
+	public ResponseEntity<ATWSettings> getUserSettings(@PathVariable("userID") int id) {
+		ATWSettings settings = db.getUserSettings(id);
+		return new ResponseEntity<ATWSettings>(settings, settings != null ? HttpStatus.OK : HttpStatus.NOT_FOUND);
+	}
+	
 }
