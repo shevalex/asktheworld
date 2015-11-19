0a8c0699fbca782577ee2d62aa8cadeed9634a2c
Project <project@Tarkits-MacBook-Air.local>
2015-11-18 21:26:55 -0500

    Fix security. Now we should have User in all rest api

--- server/src/main/java/com/pisoft/asktheworld/configurations/SecurityConfiguration.java
+++ server/src/main/java/com/pisoft/asktheworld/configurations/SecurityConfiguration.java
@@ -1,22 +1,17 @@
 package com.pisoft.asktheworld.configurations;
 
 import java.io.IOException;
-import java.util.Map;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AdviceMode;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -25,12 +20,13 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.web.filter.GenericFilterBean;
 
 import com.pisoft.asktheworld.data.DB;
 import com.pisoft.asktheworld.filters.AuthFilter;
@@ -41,30 +37,39 @@ class SecureAuthenticationEntryPoint implements AuthenticationEntryPoint {
             AuthenticationException authException) throws IOException, ServletException {
         response.sendError( HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized: Authentication token was either missing or invalid or XEP 3HAET" );
     }
-
 }
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true) 
 @EnableTransactionManagement(mode=AdviceMode.PROXY)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	SecureAuthenticationEntryPoint secureEntryPoint = new SecureAuthenticationEntryPoint();
-	
-	@Autowired AuthFilter af;
+	@Autowired
+    AuthenticationManager authManager;
+    @Autowired
+    private DB db;
+        
+    AuthFilter af = null;// = new AuthFilter();
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		af = new AuthFilter(authManager, db);
 		http
 			.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
-			.httpBasic()
-				.realmName("AskTheWorld realm")
-				.authenticationEntryPoint(secureEntryPoint)
-				.and()
+		//	.httpBasic()
+		//		.realmName("AskTheWorld realm")
+		//		.authenticationEntryPoint(secureEntryPoint)
+		//		.and()
 			.addFilterBefore(af, BasicAuthenticationFilter.class)
-			.csrf().disable(); //disable on server side
+			.csrf().disable()
+			.anonymous().disable()
+			.logout().disable()
+			
+			
+			; //disable on server side
 	}
 	
 	@Override
@@ -75,10 +80,24 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	}
 	
 	@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) {
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		//auth.inMemoryAuthentication().
-	}
-	
+	    auth.userDetailsService(userDetailsService());
+    }
+	 
+	@Bean
+	protected
+	UserDetailsService userDetailsService() {
+	    return new UserDetailsService() {
+	      @Override
+	      public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	    	  Thread.currentThread().dumpStack();
+	        return new User(username, null, true, true, true, true,
+	                null);
+	      }
+	      
+	    };
+	  }
 }
 
 
--- server/src/main/java/com/pisoft/asktheworld/filters/AuthFilter.java
+++ server/src/main/java/com/pisoft/asktheworld/filters/AuthFilter.java
@@ -17,24 +17,25 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.GrantedAuthorityImpl;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
+import com.pisoft.asktheworld.data.ATWUser;
 import com.pisoft.asktheworld.data.DB;
 
-@Component
 public class AuthFilter extends GenericFilterBean {
-
     AuthenticationManager authManager;
-    @Autowired
     private DB db;
+
     Collection<GrantedAuthority> admin;
     Collection<GrantedAuthority> user;
     Collection<GrantedAuthority> realUser;
-    @Autowired
-    public AuthFilter(AuthenticationManager authManager) {
+    
+    public AuthFilter(AuthenticationManager authManager, DB db) {
         this.authManager = authManager;
+        this.db = db;
         admin=new ArrayList<GrantedAuthority>();
         admin.add(new GrantedAuthorityImpl("ROLE_ADMIN"));
         
@@ -52,6 +53,8 @@ public class AuthFilter extends GenericFilterBean {
 
     	HttpServletRequest sr = (HttpServletRequest)request;
     	String token = sr.getHeader("token");
+    	
+    	
         if(token != null) {
             // validate the token
             String cr[] = token.split(":");
@@ -65,32 +68,36 @@ public class AuthFilter extends GenericFilterBean {
 //            + "and people.password = :password",
 //            new String[] { "username", "password" },
 //            new String[] { username, password });
-            
-            if (cr.length > 1 && db.findUser(cr[0]) != null && db.findUser(cr[0]).getPassword().equals(cr[1])) 
+
+            //Thread.dumpStack();
+            ATWUser rUser = null;
+            if (cr.length > 1 ) rUser = db.findUser(cr[0]); //TODO: replace with getById
+            if (cr.length > 1 && rUser != null && rUser.getPassword().equals(cr[1])) 
             {
             	//User exist in DB and has uses correct password
             	//TODO: remove this. It is not working properly. Need correct auth procedure 
             	//check url
+            	String pattern = "asd";
             	int index = sr.getRequestURL().indexOf("/user");
             	if(index >= 0) {
-            	String pattern = sr.getRequestURL().substring(sr.getRequestURL().indexOf("/user"));
+            	pattern = sr.getRequestURL().substring(sr.getRequestURL().indexOf("/user"));
             	System.out.println("Path info: " +pattern);
+            	} 
             	UsernamePasswordAuthenticationToken authentication;
             	// build an Authentication object with the user's info
-            	if(pattern.startsWith("/user/"+db.findUser(cr[0]).getUser_id())) {
+            	if(pattern.startsWith("/user/"+rUser.getUser_id())) {
             		System.out.println("Pattern presents");
                     authentication = 
-                            new UsernamePasswordAuthenticationToken(cr[0], cr[1], cr[0].equals("Alex") ? admin:realUser );
+                            new UsernamePasswordAuthenticationToken(cr[0], rUser, cr[0].equals("Alex") ? admin:realUser );
             	} else {
             		System.out.println("Pattern Is not presents");
                     authentication = 
-                            new UsernamePasswordAuthenticationToken(cr[0], cr[1], cr[0].equals("Alex") ? admin:user );
+                            new UsernamePasswordAuthenticationToken(cr[0], rUser, cr[0].equals("Alex") ? admin:user );
             	}
-                
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails((HttpServletRequest) request));
+            	authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails((HttpServletRequest) request));
                 // set the authentication into the SecurityContext
                 SecurityContextHolder.getContext().setAuthentication( authentication);
-            	}
+            	//}
             }
         } else {
         	System.out.println("No token in request");
